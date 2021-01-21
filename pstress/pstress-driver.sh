0bde18a5fde456c6d2df31d04bbda5f78fe29aa3
Mohit Joshi <mohit.joshi@percona.com>
2021-01-21 18:54:51 +0000

    PSTRESS-67 Started implementing the pstress_run() method - Added logic for basic setup - Added logic for datadir initialisation - Added logic for starting the server

--- pstress/pstress-driver.sh
+++ pstress/pstress-driver.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Created by Mohit Joshi
 
+# Internal variables, please do not change
+RANDOM=`date +%s%N | cut -b14-19`;
+RANDOMD=$(echo $RANDOM$RANDOM$RANDOM | sed 's/..\(......\).*/\1/');
+CONFIG_FLAG=0;INCIDENT_FLAG=0;STEP_FLAG=0;REPEAT_FLAG=0;NORMAL_MODE=0;
+RESUME_MODE=0;REPEAT_MODE=0;
+SCRIPT_PWD=$(cd `dirname $0` && pwd);
+MYSQLD_START_TIMEOUT=30;
+
 Help()
 {
 echo "usage:  $BASH_SOURCE --config <val> --incident <val> --repeat <val> --step <val>"
@@ -33,10 +41,6 @@ opts=$(getopt \
 
 eval set --$opts
 
-# Internal flags, please do not change
-CONFIG_FLAG=0;INCIDENT_FLAG=0;STEP_FLAG=0;REPEAT_FLAG=0;NORMAL_MODE=0;
-RESUME_MODE=0;REPEAT_MODE=0
-
 while true; do
     case "$1" in
     --help)
@@ -45,9 +49,9 @@ while true; do
         ;;
     --config)
         shift
-        CONFIG=$1
+        CONFIGURATION_FILE=$1
         CONFIG_FLAG=1
-        echo "Config file is: $CONFIG"
+        echo "Config file is: $CONFIGURATION_FILE"
         ;;
     --incident)
         shift
@@ -87,9 +91,66 @@ kill_server(){
   { kill -$SIG $MPID && wait $MPID; } 2>/dev/null
 }
 
+# Trap ctrl-c
+trap ctrl-c SIGINT
+
+ctrl-c(){
+  echoit "CTRL+C was pressed. Attempting to terminate running processes..."
+  KILL_PIDS=`ps -ef | grep "$RANDOMD" | grep -v "grep" | awk '{print $2}' | tr '\n' ' '`
+  if [ "${KILL_PIDS}" != "" ]; then
+    echoit "Terminating the following PID's: ${KILL_PIDS}"
+    kill -9 ${KILL_PIDS} >/dev/null 2>&1
+  fi
+  echoit "Done. Terminating pstress-driver.sh with exit code 2..."
+  exit 2
+}
+
+
 # Start normal pstress runs
 pstress_run() {
-echo "Todo: Start pstress normally"
+  TRIAL=$[ $TRIAL + 1 ]
+  SOCKET=${RUNDIR}/${TRIAL}/socket.sock
+  echoit "====== TRIAL #$TRIAL ======"
+  echoit "Ensuring there are no relevant mysqld server running"
+  KILLPID=$(ps -ef | grep "${RUNDIR}" | grep -v grep | awk '{print $2}' | tr '\n' ' ')
+  (sleep 0.2; kill -9 $KILLPID >/dev/null 2>&1; timeout -k4 -s9 4s wait $KILLPID >/dev/null 2>&1) &
+  timeout -k5 -s9 5s wait $KILLPID >/dev/null 2>&1
+  echoit "Clearing rundir..."
+  rm -Rf ${RUNDIR}/*
+  echoit "Generating new trial workdir ${RUNDIR}/${TRIAL}..."
+  mkdir -p ${RUNDIR}/${TRIAL}/data ${RUNDIR}/${TRIAL}/log
+  if [ ${TRIAL} -gt 1 ]; then
+    echoit "Copying datadir from Trial ${WORKDIR}/$((${TRIAL}-1)) into ${WORKDIR}/${TRIAL}..."
+  else
+    echoit "Copying datadir from template..."
+  fi
+  if [ `ls -l ${WORKDIR}/data.template/* | wc -l` -eq 0 ]; then
+    echoit "Assert: Something went wrong during the data directory initialisation."
+    echoit "Please check the logs at $WORKDIR/log/mysql_install_db.txt to know what went wrong. Terminating"
+    exit 1
+  elif [ ${TRIAL} -gt 1 ]; then
+    rsync -ar --exclude='*core*' ${WORKDIR}/$((${TRIAL}-1))/data/ ${RUNDIR}/${TRIAL}/data 2>&1
+  else
+    cp -R ${WORKDIR}/data.template/* ${RUNDIR}/${TRIAL}/data 2>&1
+  fi
+
+  # Start server
+  start_server
+
+  for X in $(seq 0 $mysqld_start_timeout); do
+    sleep 1
+    if [ "$MPID" == "" ]; then echoit "Assert! $MPID empty. Terminating!"; exit 1; fi
+  done
+
+  # Check if mysqld is started successfully, so pstress will run
+  if $basedir/bin/mysqladmin -uroot -S$socket ping > /dev/null 2>&1; then
+    echoit "Server started ok."
+  else
+    echoit "Server failed to start. Can not continue."
+  fi
+
+
+
 }
 
 # Repeat a particular step
@@ -109,7 +170,20 @@ echo "Todo: shutdown the server"
 
 # Start the server
 start_server() {
-echo "Todo: start the server"
+  PORT=$[50000 + ( $RANDOM % ( 9999 ) ) ]
+  SOCKET=${RUNDIR}/${TRIAL}/socket.sock
+  echoit "Starting mysqld server..."
+  CMD="${BIN} ${MYSAFE} ${MYEXTRA} --basedir=${BASEDIR} --datadir=${RUNDIR}/${TRIAL}/data --tmpdir=${RUNDIR}/${TRIAL}/tmp \
+--core-file --port=$PORT --pid_file=${RUNDIR}/${TRIAL}/pid.pid --socket=${SOCKET} \
+--log-output=none --log-error-verbosity=3 --log-error=${RUNDIR}/${TRIAL}/log/master.err"
+  echoit "$CMD"
+  $CMD > ${RUNDIR}/${TRIAL}/log/master.err 2>&1 &
+  MPID="$!"
+}
+
+# Start the PXC server
+start_pxc_server() {
+echo "Todo: start the pxc server"
 }
 
 if [[ $CONFIG_FLAG -eq 1 && $STEP_FLAG -eq 0 && $INCIDENT_FLAG -eq 0 && $REPEAT_FLAG -eq 0 ]]; then
@@ -126,13 +200,47 @@ else
   exit 0
 fi
 
+source $SCRIPT_PWD/$CONFIGURATION_FILE
+
+# Find mysqld binary
+if [ -r ${BASEDIR}/bin/mysqld ]; then
+  BIN=${BASEDIR}/bin/mysqld
+else
+  echoit "Assert: there is no (script readable) mysqld binary at ${BASEDIR}/bin/mysqld ?"
+  exit 1
+fi
+
 if [ $NORMAL_MODE -eq 1 ]; then
+  rm -Rf ${WORKDIR} ${RUNDIR}
+  mkdir ${WORKDIR} ${WORKDIR}/log ${RUNDIR}
+  echoit "Workdir: ${WORKDIR} | Rundir: ${RUNDIR} | Basedir: ${BASEDIR} "
+  echoit "mysqld Start Timeout: ${MYSQLD_START_TIMEOUT} | Client Threads: ${THREADS} | Trials: ${TRIALS} "
+  echoit "pstress Binary: ${PSTRESS_BIN}"
+  if [ "${MYINIT}" != "" ]; then echoit "MYINIT: ${MYINIT}"; fi
+  if [ "${MYSAFE}" != "" ]; then echoit "MYSAFE: ${MYSAFE}"; fi
+  if [ "${MYEXTRA}" != "" ]; then echoit "MYEXTRA: ${MYEXTRA}"; fi
+  if [ ${STORE_COPY_OF_INFILE} -eq 1 ]; then
+    echoit "Making a copy of the SQL input file used (${INFILE}) to ${WORKDIR}/ for reference..."
+    cp ${INFILE} ${WORKDIR}
+  fi
+  echoit "Making a copy of the mysqld used to ${WORKDIR}/mysqld (handy for coredump analysis and manual bundle creation)..."
+  mkdir ${WORKDIR}/mysqld
+  cp ${BIN} ${WORKDIR}/mysqld
+  echoit "Making a copy of the conf file pstress-run.conf(useful later during repeating the crashes)..."
+  cp ${SCRIPT_PWD}/pstress-run.conf ${WORKDIR}/
+  echoit "Making a copy of the seed file..."
+  echo "${SEED}" > ${WORKDIR}/seed
+  INIT_OPT="--no-defaults --initialize-insecure ${MYINIT}"
+  echoit "Generating datadir template (using mysql_install_db or mysqld --init)..."
+  ${BIN} ${INIT_OPT} --basedir=${BASEDIR} --datadir=${WORKDIR}/data.template > ${WORKDIR}/log/mysql_install_db.txt 2>&1
   echo "Starting pstress iterations"
+  TRIAL=0
   for X in $(seq 1 $TRIALS); do
     pstress_run
   done
 elif [ $RESUME_MODE -eq 1 ]; then
-  echo "Resuming pstress iterations after step:$TRIAL"
+  echo "Resuming pstress iterations after step:$STEP"
+  TRIAL=$STEP
   LEFT_TRIALS=$[ $TRIALS - $TRIAL ]
   for X in $(seq 1 $LEFT_TRIALS); do
     resume_pstress_run
