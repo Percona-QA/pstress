cc1527cf792565beb13558ec340878239326ff94
Puneet Kaushik <puneet.kaushik@percona.com>
2023-06-28 16:53:44 +0530

    Updates

--- pstress/generate_wsrep_options.sh
+++ pstress/generate_wsrep_options.sh
@@ -3,7 +3,7 @@
 # Created by Puneet Kaushik, Percona LLC                                               #
 # Created on: 28-JUNE-2023                                                           #
 #                                                                                   #
-# Purpose: This script auto-generates the rocksdb_options.txt file                  #
+# Purpose: This script auto-generates the wsrep_options.txt file                  #
 # Usage: ./generate_wsrep_options.sh <path to mysql install/base dir>             #
 # References: https://docs.percona.com/percona-xtradb-cluster/8.0/wsrep-system-index.html #
 #####################################################################################
@@ -55,9 +55,10 @@ $BASEDIR/bin/mysql --no-defaults -S$SOCKET -uroot -e "SHOW VARIABLES LIKE '%wsre
 # Remove the first line from the temp file
 # Variable_name, Variable_value (we do not need the column name displayed in temp file)
 sed -i '1d;6d' $TEMP_FILE
+sed -i 's/_/\-/g' $TEMP_FILE
 
 # List of rocksdb variables which must not be changed.
-EXCLUDED_LIST=( --wsrep_applier_FK_checks --wsrep_applier_UK_checks --wsrep_causal_reads --wsrep_cluster_address --wsrep_cluster_name --wsrep_data_home_dir --wsrep_dbug_option --wsrep_drupal_282555_workaround --wsrep_forced_binlog_format --wsrep_ignore_apply_errors --wsrep_min_log_verbosity --wsrep_node_address --wsrep_node_incoming_address --wsrep_node_name --wsrep_notify_cmd --wsrep_provider --wsrep_provider_options --wsrep_restart_slave --wsrep_slave_FK_checks --wsrep_slave_threads --wsrep_slave_UK_checks --wsrep_sst_donor --wsrep_sst_method --wsrep_sst_receive_address --wsrep_start_position --wsrep_disk_pages_encrypt --wsrep_gcache_encrypt --wsrep_SR_store --wsrep_sst_allowed_methods)
+EXCLUDED_LIST=( --wsrep-applier-FK-checks --wsrep-applier-UK-checks --wsrep-causal-reads --wsrep-cluster-address --wsrep-cluster-name --wsrep-data-home-dir --wsrep-dbug-option --wsrep-drupal-282555-workaround --wsrep-forced-binlog-format --wsrep-ignore-apply-errors --wsrep-min-log-verbosity --wsrep-node-address --wsrep-node-incoming-address --wsrep-node-name --wsrep-notify-cmd --wsrep-provider --wsrep-provider-options --wsrep-restart-slave --wsrep-slave-FK-checks --wsrep-slave-threads --wsrep-slave-UK-checks --wsrep-sst-donor --wsrep-sst-method --wsrep-sst-receive-address --wsrep-start-position --wsrep-disk-pages-encrypt --wsrep-gcache-encrypt --wsrep-SR-store --wsrep-sst-allowed-methods)
 
 # Create an output file which contains all the options/values
 rm -rf $OUTPUT_FILE
@@ -73,69 +74,69 @@ while read line; do
   fi
   if [[ " ${EXCLUDED_LIST[@]} " =~ " ${OPTION} " ]]; then
     echoit "Option '$OPTION' is logically excluded from being handled by this script..."
-  elif [[ "$OPTION" == "--pxc_encrypt_cluster_traffic" ]]; then
+  elif [[ "$OPTION" == "--pxc-encrypt-cluster-traffic" ]]; then
     echoit " > Adding possible values 0 and 1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--pxc_maint_mode" ]]; then
+  elif [[ "$OPTION" == "--pxc-maint-mode" ]]; then
     echoit " > Adding possible values disabled, shutdown and maintainance for option '${OPTION}' to the final list..."
     echo "$OPTION=DISABLED" >> $OUTPUT_FILE
     echo "$OPTION=SHUTDOWN" >> $OUTPUT_FILE  
     echo "$OPTION=MAINTENANCE" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--pxc_maint_transition_period" ]]; then
+  elif [[ "$OPTION" == "--pxc-maint-transition-period" ]]; then
     echoit " > Adding possible values 0,2,10,16 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=2" >> $OUTPUT_FILE
     echo "$OPTION=10" >> $OUTPUT_FILE
     echo "$OPTION=16" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--pxc_strict_mode" ]]; then
+ elif [[ "$OPTION" == "--pxc-strict-mode" ]]; then
     echoit " > Adding possible values DISABLED, PERMISSIVE, ENFORCING, MASTER for option '${OPTION}' to the final list..."
     echo "$OPTION=ENFORCING" >> $OUTPUT_FILE
     echo "$OPTION=DISABLED" >> $OUTPUT_FILE
     echo "$OPTION=PERMISSIVE" >> $OUTPUT_FILE
     echo "$OPTION=MASTER" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_applier_threads" ]]; then
+ elif [[ "$OPTION" == "--wsrep-applier-threads" ]]; then
     echoit " > Adding possible values 1,2,12,24 for option '${OPTION}' to the final list..."
     echo "$OPTION=1" >> $OUTPUT_FILE
     echo "$OPTION=2" >> $OUTPUT_FILE
     echo "$OPTION=12" >> $OUTPUT_FILE
     echo "$OPTION=24" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_auto_increment_control" ]]; then
+ elif [[ "$OPTION" == "--wsrep-auto-increment-control" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_certification_rules" ]]; then
+ elif [[ "$OPTION" == "--wsrep-certification-rules" ]]; then
     echoit " > Adding possible values STRICT, OPTIMIZED for option '${OPTION}' to the final list..."
     echo "$OPTION=STRICT" >> $OUTPUT_FILE
     echo "$OPTION=OPTIMIZED" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_certify_nonPK" ]]; then
+ elif [[ "$OPTION" == "--wsrep-certify-nonPK" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_debug" ]]; then
+ elif [[ "$OPTION" == "--wsrep-debug" ]]; then
     echoit " > Adding possible values NONE, SERVER, TRANSACTION, STREAMING , CLIENT  for option '${OPTION}' to the final list..."
     echo "$OPTION=NONE" >> $OUTPUT_FILE
     echo "$OPTION=SERVER" >> $OUTPUT_FILE
     echo "$OPTION=TRANSACTION" >> $OUTPUT_FILE
     echo "$OPTION=STREAMING" >> $OUTPUT_FILE
     echo "$OPTION=CLIENT" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_desync" ]]; then
+ elif [[ "$OPTION" == "--wsrep-desync" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_dirty_reads" ]]; then
+ elif [[ "$OPTION" == "--wsrep-dirty-reads" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_load_data_splitting" ]]; then
+ elif [[ "$OPTION" == "--wsrep-load-data-splitting" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_log_conflicts" ]]; then
+ elif [[ "$OPTION" == "--wsrep-log-conflicts" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_max_ws_rows" ]]; then
+ elif [[ "$OPTION" == "--wsrep-max-ws-rows" ]]; then
     echoit " > Adding possible values 0,1,2,12,254,1023,2047,1048576 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
@@ -145,7 +146,7 @@ while read line; do
     echo "$OPTION=1023" >> $OUTPUT_FILE
     echo "$OPTION=2047" >> $OUTPUT_FILE
     echo "$OPTION=1048576" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_max_ws_size" ]]; then
+ elif [[ "$OPTION" == "--wsrep-max-ws-size" ]]; then
     echoit " > Adding possible values 0,1,2,12,254,1023,2047,1048576 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
@@ -156,47 +157,47 @@ while read line; do
     echo "$OPTION=2047" >> $OUTPUT_FILE
     echo "$OPTION=1048576" >> $OUTPUT_FILE
     echo "$OPTION=2147483647" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_mode" ]]; then   
+ elif [[ "$OPTION" == "--wsrep-mode" ]]; then
     echoit " > Adding possible values Empty, IGNORE_NATIVE_REPLICATION_FILTER_RULES for option '${OPTION}' to the final list..."
     echo "$OPTION=EMPTY" >> $OUTPUT_FILE
     echo "$OPTION=IGNORE_NATIVE_REPLICATION_FILTER_RULES" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_on" ]]; then
+ elif [[ "$OPTION" == "--wsrep-on" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_OSU_method" ]]; then
-    echoit " > Adding possible values TOI, RSU and NBO for option '${OPTION}' to the final list..."
+ elif [[ "$OPTION" == "--wsrep-OSU-method" ]]; then
+    echoit " > Adding possible values TOI, RSU, NBO for option '${OPTION}' to the final list..."
     echo "$OPTION=TOI" >> $OUTPUT_FILE
     echo "$OPTION=RSU" >> $OUTPUT_FILE
     echo "$OPTION=NBO" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_recover" ]]; then
+ elif [[ "$OPTION" == "--wsrep-recover" ]]; then
     echoit " > Adding possible values 0,1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_reject_queries" ]]; then
+ elif [[ "$OPTION" == "--wsrep-reject-queries" ]]; then
     echoit " > Adding possible values NONE, ALL , ALL_KILL for option '${OPTION}' to the final list..."
     echo "$OPTION=NONE" >> $OUTPUT_FILE
     echo "$OPTION=ALL" >> $OUTPUT_FILE
     echo "$OPTION=ALL_KILL" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_replicate_myisam" ]]; then
+ elif [[ "$OPTION" == "--wsrep-replicate-myisam" ]]; then
     echoit " > Adding possible values 0, 1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_restart_replica" ]]; then
+ elif [[ "$OPTION" == "--wsrep-restart-replica" ]]; then
     echoit " > Adding possible values 0, 1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_retry_autocommit" ]]; then
+ elif [[ "$OPTION" == "--wsrep-retry-autocommit" ]]; then
     echoit " > Adding possible values 0, 1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_RSU_commit_timeout" ]]; then
+ elif [[ "$OPTION" == "--wsrep-RSU-commit-timeout" ]]; then
     echoit " > Adding possible values 5000, 10000, 250000, 31536000000000 for option '${OPTION}' to the final list..."
     echo "$OPTION=5000" >> $OUTPUT_FILE
     echo "$OPTION=10000" >> $OUTPUT_FILE
     echo "$OPTION=250000" >> $OUTPUT_FILE
     echo "$OPTION=31536000000000" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_sync_wait" ]]; then
+ elif [[ "$OPTION" == "--wsrep-sync-wait" ]]; then
     echoit " > Adding possible values 0,1,2,3,4,5,6,7 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
@@ -206,7 +207,7 @@ while read line; do
     echo "$OPTION=5" >> $OUTPUT_FILE
     echo "$OPTION=6" >> $OUTPUT_FILE
     echo "$OPTION=7" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_trx_fragment_size" ]]; then
+ elif [[ "$OPTION" == "--wsrep-trx-fragment-size" ]]; then
     echoit " > Adding possible values 0,1,2,12,254,1023,2047,1048576 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE
@@ -217,12 +218,12 @@ while read line; do
     echo "$OPTION=2047" >> $OUTPUT_FILE
     echo "$OPTION=1048576" >> $OUTPUT_FILE
     echo "$OPTION=2147483647" >> $OUTPUT_FILE	 
- elif [[ "$OPTION" == "--wsrep_trx_fragment_unit" ]]; then
+ elif [[ "$OPTION" == "--wsrep-trx-fragment-unit" ]]; then
     echoit " > Adding possible values bytes, rows , statements for option '${OPTION}' to the final list..."
     echo "$OPTION=bytes" >> $OUTPUT_FILE
     echo "$OPTION=rows" >> $OUTPUT_FILE
     echo "$OPTION=statements" >> $OUTPUT_FILE
- elif [[ "$OPTION" == "--wsrep_sst_donor_rejects_queries" ]]; then
+ elif [[ "$OPTION" == "--wsrep-sst-donor-rejects-queries" ]]; then
     echoit " > Adding possible values 0, 1 for option '${OPTION}' to the final list..."
     echo "$OPTION=0" >> $OUTPUT_FILE
     echo "$OPTION=1" >> $OUTPUT_FILE   
@@ -235,5 +236,4 @@ while read line; do
   fi
 done < $TEMP_FILE
 rm -rf $TEMP_FILE
-sed -i 's/_/\-/g' $OUTPUT_FILE
 echo "Done! Output file: ${OUTPUT_FILE}"
