eb1c2e7f88a66eabac9e84503d52c7c3c21ce647
rahul malik <contact.rmalik@gmail.com>
2024-08-27 15:18:41 +0200

    Update common.hpp

--- src/common.hpp
+++ src/common.hpp
@@ -6,9 +6,9 @@
 #endif
 
 #ifdef MAXPACKET
-  #ifndef MAX_PACKET_DEFAULT
-  #define MAX_PACKET_DEFAULT 4194304
-  #endif
+#ifndef MAX_PACKET_DEFAULT
+#define MAX_PACKET_DEFAULT 4194304
+#endif
 #endif
 
 #ifndef FORK
@@ -88,6 +88,7 @@ struct Option {
     NO_INSERT,
     NO_UPDATE,
     NO_DELETE,
+    ONLY_SELECT,
     SELECT_ALL_ROW,
     SELECT_ROW_USING_PKEY,
     INSERT_RANDOM_ROW,
@@ -159,13 +160,49 @@ struct Option {
     FUNCTION_CONTAINS_DML,
     OPTION_PROB_FILE,
     NO_TIMESTAMP,
-    NO_DATE,
-    NO_DATETIME,
-    NO_BIT,
     COMPARE_RESULT,
     SECONDARY_ENGINE,
-    SLEEP_AFTER_CREATE_TABLE,
+    WAIT_FOR_SYNC,
+    ALTER_SECONDARY_ENGINE,
+    NO_FK_CASCADE,
+    ENFORCE_MERGE,
+    REWRITE_ROW_GROUP_MIN_ROWS,
+    REWRITE_ROW_GROUP_MAX_BYTES,
+    REWRITE_ROW_GROUP_MAX_ROWS,
+    REWRITE_DELTA_NUM_ROWS,
+    REWRITE_DELTA_NUM_UNDO,
+    REWRITE_GC,
+    REWRITE_BLOCKING,
+    REWRITE_MAX_ROW_ID_HASH_MAP,
+    REWRITE_FORCE,
+    REWRITE_NO_RESIDUAL,
+    REWRITE_MAX_INTERNAL_BLOB_SIZE,
+    REWRITE_BLOCK_COOKER_ROW_GROUP_MAX_ROWS,
+    REWRITE_PARTIAL,
+    NOT_SECONDARY,
+    MODIFY_COLUMN_SECONDARY_ENGINE,
+    NO_DATE,
+    NO_DATETIME,
+    NO_TEXT,
+    NO_CHAR,
+    NO_VARCHAR,
+    NO_FLOAT,
+    NO_DOUBLE,
+    NO_BOOL,
+    NO_INTEGER,
+    NO_INT,
+    ONLY_INT,
+    NO_BIT,
+    NULL_PROB,
+    SECONDARY_AFTER_CREATE,
+    DELAY_IN_SECONDARY,
+    SECONDARY_GC,
+    SELECT_IN_SECONDARY,
     EXACT_COLUMNS,
+    EXACT_INDEXES,
+    POSITIVE_INT_PROB,
+    PLAIN_REWRITE,
+    USING_PK_PROB,
     MAX
   } option;
   Option(Type t, Opt o, std::string n)
@@ -194,27 +231,21 @@ struct Option {
     }
   }
 
-  void setBool(bool in) {
-    default_bool = in;
-  }
+  void setBool(bool in) { default_bool = in; }
   void setInt(std::string n) { default_int = std::stol(n); }
-  void setInt(int n) {
-    default_int = n;
-  }
-  void setString(std::string n) {
-    default_value = n;
-  };
+  void setInt(int n) { default_int = n; }
+  void setString(std::string n) { default_value = n; };
   void setSQL() { sql = true; };
   void setDDL() { ddl = true; };
   void set_cl() { cl = true; }
 
   std::string name;
   std::string help;
-  std::string default_value;
-  long int default_int; // if default value is integer
-  bool default_bool; // if default value is bool
-  bool sql; // true if option is SQL, False if others
-  bool ddl; // If SQL is DDL, or false if it is not
+  std::string default_value = "";
+  long int default_int = 0;       // if default value is integer
+  bool default_bool = false;      // if default value is bool
+  bool sql;                       // true if option is SQL, False if others
+  bool ddl;                       // If SQL is DDL, or false if it is not
   bool cl = false;                // set if it was pass trough command line
   short args = required_argument; // default is required argument
   std::atomic<unsigned long int> total_queries;   // totatl times executed
@@ -234,7 +265,6 @@ typedef std::vector<Option *> Opx;
 typedef std::vector<Server_Option *> Ser_Opx;
 extern Opx *options;
 extern Ser_Opx *server_options;
-extern const char *binary_fullpath;
 void add_options();
 void add_server_options(std::string str);
 void add_server_options_file(std::string file_name);
