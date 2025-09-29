641f85f8e4d53b30717faeed36088470634eef3d
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2025-09-29 16:52:29 +0200

    PSTRESS-160: Improve `generate_rocksdb_options.sh` and option files
    1. Remove default binary options (`ON` or `OFF`) from `rocksdb_options_8042.txt`.
    2. Fix some option ranges after fixes in PS-10210 for `rocksdb_options_8042.txt`.
    3. Fix `rocksdb_default_cf_options` which doesn't work with quotation marks.
    4. Add `rocksdb_no_block_cache` and `rocksdb_create_if_missing` to `EXCLUDED_LIST`.
    5. Add required depended options for testing `rocksdb_persistent_cache_size_mb`, `rocksdb_index_type`, `rocksdb_enable_pipelined_write`, `rocksdb_write_disable_wal`, `rocksdb_allow_mmap_writes`

--- pstress/generate_rocksdb_options.sh
+++ pstress/generate_rocksdb_options.sh
@@ -9,15 +9,16 @@
 #####################################################################################
 
 # Script expected argument
-BASEDIR=$1
+BASEDIR=${BASEDIR:-$1}
 
 # User variables
-OUTPUT_FILE=/tmp/rocksdb_options_80.txt
+OUTPUT_PATH=${OUTPUT_PATH:-/tmp/gen_rocksdb_options}
+OUTPUT_FILE=${OUTPUT_PATH}/rocksdb_options_80.txt
 
 # Internal variables, do not change
-TEMP_FILE=/tmp/rocksdb_options_80.tmp
+TEMP_FILE=${OUTPUT_PATH}/rocksdb_options_80.tmp
 PORT=22000
-SOCKET=/tmp/mysql_22000.sock
+SOCKET=${OUTPUT_PATH}/mysql_22000.sock
 DATADIR=$BASEDIR/data
 MYSQLD_START_TIMEOUT=30
 
@@ -25,19 +26,20 @@ MYSQLD_BIN=$BASEDIR/bin/mysqld
 # Check if mysqld binaries exists
 if [ ! -x ${MYSQLD_BIN} ]; then MYSQLD_BIN=$BASEDIR/bin/mysqld-debug; fi
 if [ ! -x ${MYSQLD_BIN} ]; then
-    echo "Error: no $BASEDIR/bin/mysqld or $MYSQLD_BIN found!"
+    echo "Error: BASEDIR=$BASEDIR; no $BASEDIR/bin/mysqld or $MYSQLD_BIN found!"
     exit 1
 fi
 
 # Display function
 echoit(){
   echo "[$(date +'%T')] $1"
-  echo "[$(date +'%T')] $1" >> /tmp/generate_rocksdb_options.log
+  echo "[$(date +'%T')] $1" >> ${OUTPUT_PATH}/generate_rocksdb_options.log
 }
 
+mkdir -p ${OUTPUT_PATH}
 echoit "Starting MySQL server"
-$MYSQLD_BIN --no-defaults --datadir=$DATADIR --initialize-insecure > /tmp/mysql_install_db.txt 2>&1
-$MYSQLD_BIN --no-defaults --datadir=$DATADIR --port=$PORT --socket=$SOCKET 2>&1 > /tmp/master.err 2>&1 &
+$MYSQLD_BIN --no-defaults --datadir=$DATADIR --initialize-insecure > ${OUTPUT_PATH}/mysql_install_db.txt 2>&1
+$MYSQLD_BIN --no-defaults --datadir=$DATADIR --port=$PORT --socket=$SOCKET 2>&1 > ${OUTPUT_PATH}/master.err 2>&1 &
 MPID=$!
 
 echoit "Waiting for the server to fully start..."
@@ -46,7 +48,7 @@ for X in $(seq 0 ${MYSQLD_START_TIMEOUT}); do
   if [ "$MPID" == "" ]; then echoit "Assert! ${MPID} empty. Terminating!"; exit 1; fi
   if $BASEDIR/bin/mysqladmin -uroot -S${SOCKET} ping > /dev/null 2>&1; then
     echoit "MySQL server started successfully"
-    $BASEDIR/bin/ps-admin --enable-rocksdb -uroot -S/tmp/mysql_22000.sock 2>&1 > /dev/null
+    $BASEDIR/bin/ps-admin --enable-rocksdb -uroot -S${OUTPUT_PATH}/mysql_22000.sock 2>&1 > /dev/null
     echoit "RocksDB loaded successfully"
     break
   fi
@@ -70,7 +72,7 @@ $BASEDIR/bin/mysql --no-defaults -S$SOCKET -uroot -e "
 sed -i 1d $TEMP_FILE
 
 # List of rocksdb variables which must not be changed.
-EXCLUDED_LIST=( rocksdb_write_batch_max_bytes rocksdb_disable_instant_ddl rocksdb_column_default_value_as_expression rocksdb_cancel_manual_compactions rocksdb_alter_table_comment_inplace rocksdb_io_error_action rocksdb_invalid_create_option_action rocksdb_corrupt_data_action rocksdb_fs_uri rocksdb_compact_cf rocksdb_info_log_level rocksdb_update_cf_options rocksdb_delete_cf rocksdb_override_cf_options rocksdb_create_checkpoint rocksdb_create_temporary_checkpoint rocksdb_fault_injection_options rocksdb_read_free_rpl_tables rocksdb_persistent_cache_path rocksdb_strict_collation_exceptions rocksdb_tmpdir rocksdb_trace_block_cache_access rocksdb_trace_queries rocksdb_wal_dir rocksdb_wsenv_path rocksdb_datadir )
+EXCLUDED_LIST=( rocksdb_no_block_cache rocksdb_create_if_missing rocksdb_write_batch_max_bytes rocksdb_disable_instant_ddl rocksdb_column_default_value_as_expression rocksdb_cancel_manual_compactions rocksdb_alter_table_comment_inplace rocksdb_io_error_action rocksdb_invalid_create_option_action rocksdb_corrupt_data_action rocksdb_fs_uri rocksdb_compact_cf rocksdb_info_log_level rocksdb_update_cf_options rocksdb_delete_cf rocksdb_override_cf_options rocksdb_create_checkpoint rocksdb_create_temporary_checkpoint rocksdb_fault_injection_options rocksdb_read_free_rpl_tables rocksdb_persistent_cache_path rocksdb_strict_collation_exceptions rocksdb_tmpdir rocksdb_trace_block_cache_access rocksdb_trace_queries rocksdb_wal_dir rocksdb_wsenv_path rocksdb_datadir )
 
 # Create an output file which contains all the options/values
 rm -rf $OUTPUT_FILE
@@ -107,64 +109,71 @@ while read line; do
   fi
   if [[ " ${EXCLUDED_LIST[@]} " =~ " ${COMMAND} " ]]; then
     echoit "Option '$OPTION' is logically excluded from being handled by this script..."
-  elif [[ "$OPTION" == "--loose-rocksdb_access_hint_on_compaction_start" ]]; then
+  elif [[  "$COMMAND" == "rocksdb_persistent_cache_size_mb" ]]; then
+    echoit " > Adding possible values for option '${OPTION}' to the final list..."
+    echo "$OPTION=100 --loose-rocksdb_persistent_cache_path=." >> $OUTPUT_FILE
+    echo "$OPTION=254 --loose-rocksdb_persistent_cache_path=." >> $OUTPUT_FILE
+    echo "$OPTION=1023 --loose-rocksdb_persistent_cache_path=." >> $OUTPUT_FILE
+    echo "$OPTION=2047 --loose-rocksdb_persistent_cache_path=." >> $OUTPUT_FILE
+    echo "$OPTION=4096 --loose-rocksdb_persistent_cache_path=." >> $OUTPUT_FILE
+  elif [[ "$COMMAND" == "rocksdb_access_hint_on_compaction_start" ]]; then
     echoit " > Adding possible values NONE, NORMAL, SEQUENTIAL, WILLNEED for option '${OPTION}' to the final list..."
     echo "$OPTION=NONE" >> $OUTPUT_FILE
     echo "$OPTION=NORMAL" >> $OUTPUT_FILE
     echo "$OPTION=SEQUENTIAL" >> $OUTPUT_FILE
     echo "$OPTION=WILLNEED" >> $OUTPUT_FILE
-  elif [[  "$OPTION" == "--loose-rocksdb_index_type" ]]; then
+  elif [[  "$COMMAND" == "rocksdb_index_type" ]]; then
     echoit " > Adding possible values kBinarySearch, kHashSearch for option '${OPTION}' to the final list..."
     echo "$OPTION=kBinarySearch" >> $OUTPUT_FILE
-    echo "$OPTION=kHashSearch" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--loose-rocksdb_write_policy" ]]; then
+    echo "$OPTION=kHashSearch --loose-rocksdb_default_cf_options=prefix_extractor=capped:12" >> $OUTPUT_FILE
+  elif [[ "$COMMAND" == "rocksdb_write_policy" ]]; then
     echoit " > Adding possible values write_committed, write_prepared, write_unprepared for option '${OPTION}' to the final list..."
     echo "$OPTION=write_committed" >> $OUTPUT_FILE
     echo "$OPTION=write_prepared" >> $OUTPUT_FILE
     echo "$OPTION=write_unprepared" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--loose-rocksdb_manual_compaction_bottommost_level" ]]; then
+  elif [[ "$COMMAND" == "rocksdb_manual_compaction_bottommost_level" ]]; then
     echoit " > Adding possible values kSkip, kIfHaveCompactionFilter, kForce, kForceOptimized for option '${OPTION}' to the final list..."
     echo "$OPTION=kSkip" >> $OUTPUT_FILE
     echo "$OPTION=kIfHaveCompactionFilter" >> $OUTPUT_FILE
     echo "$OPTION=kForce" >> $OUTPUT_FILE
     echo "$OPTION=kForceOptimized" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--loose-rocksdb_cache_high_pri_pool_ratio" ]]; then
+  elif [[ "$COMMAND" == "rocksdb_cache_high_pri_pool_ratio" ]]; then
     echoit " > Adding possible values 0, 0.25, 0.5, 0.75, 1.0 for option '${OPTION}' to the final list..."
     echo "$OPTION=0.0" >> $OUTPUT_FILE
     echo "$OPTION=0.25" >> $OUTPUT_FILE
     echo "$OPTION=0.50" >> $OUTPUT_FILE
     echo "$OPTION=0.75" >> $OUTPUT_FILE
     echo "$OPTION=1.0" >> $OUTPUT_FILE
-  elif [[ "$OPTION" == "--loose-rocksdb_default_cf_options" ]]; then
+  elif [[ "$COMMAND" == "rocksdb_default_cf_options" ]]; then
     echoit " > Adding possible values for option '${OPTION}' to the final list..."
-    echo "$OPTION=\"write_buffer_size=256m;max_write_buffer_number=1\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"write_buffer_size=32m;max_write_buffer_number=4\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"write_buffer_size=128m;max_write_buffer_number=3\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"target_file_size_base=32m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"target_file_size_base=128m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"target_file_size_base=256m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_bytes_for_level_base=32m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_bytes_for_level_base=128m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_bytes_for_level_base=256m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_bytes_for_level_base=512m\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_write_buffer_number=4\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_write_buffer_number=3\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_write_buffer_number=2\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"max_write_buffer_number=1\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_file_num_compaction_trigger=4\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_file_num_compaction_trigger=3\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_file_num_compaction_trigger=2\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_slowdown_writes_trigger=20;level0_stop_writes_trigger=10\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_slowdown_writes_trigger=10;level0_stop_writes_trigger=20\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level0_slowdown_writes_trigger=1;level0_stop_writes_trigger=3\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"compression=kLZ4Compression;bottommost_compression=kLZ4Compression;compression_opts=-14:4:0\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"compression=kZSTD;bottommost_compression=kZSTD;compression_opts=-14:4:0\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"compression=kZlibCompression;bottommost_compression=kZlibCompression;compression_opts=-14:4:0\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level_compaction_dynamic_level_bytes=true;optimize_filters_for_hits=true;memtable_prefix_bloom_size_ratio=0.05\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level_compaction_dynamic_level_bytes=false;optimize_filters_for_hits=true;memtable_prefix_bloom_size_ratio=0.01\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"level_compaction_dynamic_level_bytes=true;optimize_filters_for_hits=false;memtable_prefix_bloom_size_ratio=0.10\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"compaction_pri=kMinOverlappingRatio;prefix_extractor=capped:12\"" >> $OUTPUT_FILE
-    echo "$OPTION=\"block_based_table_factory={cache_index_and_filter_blocks=1;filter_policy=bloomfilter:10:false;whole_key_filtering=0}\"" >> $OUTPUT_FILE
+    echo "$OPTION=write_buffer_size=256m;max_write_buffer_number=1" >> $OUTPUT_FILE
+    echo "$OPTION=write_buffer_size=32m;max_write_buffer_number=4" >> $OUTPUT_FILE
+    echo "$OPTION=write_buffer_size=128m;max_write_buffer_number=3" >> $OUTPUT_FILE
+    echo "$OPTION=target_file_size_base=32m" >> $OUTPUT_FILE
+    echo "$OPTION=target_file_size_base=128m" >> $OUTPUT_FILE
+    echo "$OPTION=target_file_size_base=256m" >> $OUTPUT_FILE
+    echo "$OPTION=max_bytes_for_level_base=32m" >> $OUTPUT_FILE
+    echo "$OPTION=max_bytes_for_level_base=128m" >> $OUTPUT_FILE
+    echo "$OPTION=max_bytes_for_level_base=256m" >> $OUTPUT_FILE
+    echo "$OPTION=max_bytes_for_level_base=512m" >> $OUTPUT_FILE
+    echo "$OPTION=max_write_buffer_number=4" >> $OUTPUT_FILE
+    echo "$OPTION=max_write_buffer_number=3" >> $OUTPUT_FILE
+    echo "$OPTION=max_write_buffer_number=2" >> $OUTPUT_FILE
+    echo "$OPTION=max_write_buffer_number=1" >> $OUTPUT_FILE
+    echo "$OPTION=level0_file_num_compaction_trigger=4" >> $OUTPUT_FILE
+    echo "$OPTION=level0_file_num_compaction_trigger=3" >> $OUTPUT_FILE
+    echo "$OPTION=level0_file_num_compaction_trigger=2" >> $OUTPUT_FILE
+    echo "$OPTION=level0_slowdown_writes_trigger=20;level0_stop_writes_trigger=10" >> $OUTPUT_FILE
+    echo "$OPTION=level0_slowdown_writes_trigger=10;level0_stop_writes_trigger=20" >> $OUTPUT_FILE
+    echo "$OPTION=level0_slowdown_writes_trigger=1;level0_stop_writes_trigger=3" >> $OUTPUT_FILE
+    echo "$OPTION=compression=kLZ4Compression;bottommost_compression=kLZ4Compression;compression_opts=-14:4:0" >> $OUTPUT_FILE
+    echo "$OPTION=compression=kZSTD;bottommost_compression=kZSTD;compression_opts=-14:4:0" >> $OUTPUT_FILE
+    echo "$OPTION=compression=kZlibCompression;bottommost_compression=kZlibCompression;compression_opts=-14:4:0" >> $OUTPUT_FILE
+    echo "$OPTION=level_compaction_dynamic_level_bytes=true;optimize_filters_for_hits=true;memtable_prefix_bloom_size_ratio=0.05" >> $OUTPUT_FILE
+    echo "$OPTION=level_compaction_dynamic_level_bytes=false;optimize_filters_for_hits=true;memtable_prefix_bloom_size_ratio=0.01" >> $OUTPUT_FILE
+    echo "$OPTION=level_compaction_dynamic_level_bytes=true;optimize_filters_for_hits=false;memtable_prefix_bloom_size_ratio=0.10" >> $OUTPUT_FILE
+    echo "$OPTION=compaction_pri=kMinOverlappingRatio;prefix_extractor=capped:12" >> $OUTPUT_FILE
+    echo "$OPTION=block_based_table_factory={cache_index_and_filter_blocks=1;filter_policy=bloomfilter:10:false;whole_key_filtering=0}" >> $OUTPUT_FILE
   elif [[ "$OPTION" == "--loose-rocksdb_read_free_rpl" ]]; then
     echoit " > Adding possible values OFF, PK_SK, PK_ONLY for option '${OPTION}' to the final list..."
     echo "$OPTION=OFF" >> $OUTPUT_FILE
@@ -213,10 +222,25 @@ while read line; do
     echo "$OPTION=5000" >> $OUTPUT_FILE
     echo "$OPTION=1999999" >> $OUTPUT_FILE
     echo "$OPTION=2000000" >> $OUTPUT_FILE
+  elif [[ "$COMMAND" == "rocksdb_enable_pipelined_write" ]]; then
+    if [ "$VALUE" == "OFF" ]; then
+      echo "$OPTION=ON --loose-rocksdb_two_write_queues=OFF" >> $OUTPUT_FILE
+    else
+      echo "$OPTION=OFF --loose-rocksdb_two_write_queues=OFF" >> $OUTPUT_FILE
+    fi
+  elif [[ "$COMMAND" == "rocksdb_write_disable_wal" || "$COMMAND" == "rocksdb_allow_mmap_writes" ]]; then
+    if [ "$VALUE" == "OFF" ]; then
+      echo "$OPTION=ON --loose-rocksdb_flush_log_at_trx_commit=0" >> $OUTPUT_FILE
+    else
+      echo "$OPTION=OFF --loose-rocksdb_flush_log_at_trx_commit=0" >> $OUTPUT_FILE
+    fi
   elif [ "$VALUE" == "ON" -o "$VALUE" == "OFF" ]; then
     echoit " > Adding possible values ON, OFF for option '${OPTION}' to the final list..."
-    echo "$OPTION=ON" >> $OUTPUT_FILE
-    echo "$OPTION=OFF" >> $OUTPUT_FILE
+    if [ "$VALUE" == "OFF" ]; then
+      echo "$OPTION=ON" >> $OUTPUT_FILE
+    else
+      echo "$OPTION=OFF" >> $OUTPUT_FILE
+    fi
   elif [[ "$VALUE" =~ ^-?[0-9]+$ ]]; then
     echoit "  > Adding int values ( $VALUE, 0, 1, -1, 2, 12, 24, 254, 1023, 2047, 2147483647, 1125899906842624, 18446744073709551615 ) for option '${OPTION}' to the final list..."
     fix_min_value
@@ -246,6 +270,6 @@ while read line; do
   fi
 done < $TEMP_FILE
 
-rm -rf $TEMP_FILE
+#rm -rf $TEMP_FILE
 echo "Done! Output file: ${OUTPUT_FILE}"
 killall -9 $MYSQLD_BIN
